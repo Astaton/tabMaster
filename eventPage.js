94dc90b75e08bc81b5e232ae826cd24331015389
Astaton <orson108@hotmail.com>
2020-05-02 22:40:35 -0400

    update -started creating context menus

--- eventPage.js
+++ eventPage.js
@@ -1,11 +1,30 @@
-window.pages = {};
+// window.pages = {};
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (!window.pages[request.url]) {
-        window.pages[request.url] = true;
-    }
-});
+// chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+//     if (!window.pages[request.url]) {
+//         window.pages[request.url] = true;
+//     }
+// });
 
-chrome.browserAction.onClicked.addListener((tab) => {
-    chrome.tabs.create({url: 'popup.html'})
-})
+// chrome.browserAction.onClicked.addListener((tab) => {
+//     chrome.tabs.create({url: 'popup.html'})
+// })
+
+const contextMenuItem = {
+    "id": "addTab",
+    "title": "Add tab to a tab group",
+    "contexts": ["page"]
+}
+
+const testContextMenu = {
+    "id": "test1",
+    "parentId": "addTab",
+    "title": "test1",
+    "contexts": ["page"] 
+}
+
+chrome.contextMenus.create(contextMenuItem);
+chrome.contextMenus.onClicked.addListener;
+
+chrome.contextMenus.create(testContextMenu);
+
